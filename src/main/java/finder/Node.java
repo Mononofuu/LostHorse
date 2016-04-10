58c426dc02a8de40e67a255c1b42313f71b69010
Mononofuu <mononofuu@gmail.com>
2016-04-10 18:39:02 +0300

    - done, but not optimized

--- src/main/java/finder/Node.java
+++ src/main/java/finder/Node.java
@@ -1,19 +1,109 @@
 package finder;
 
-public class Node extends AbstractNode {
+public class Node {
 
-    public Node(int xPosition, int yPosition) {
-        super(xPosition, yPosition);
+    private final static int BASICMOVEMENTCOST = 10;
+
+    private int x;
+    private int y;
+    private boolean walkable;
+    private Node previous;
+    private int gCosts;
+    private int hCosts;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.walkable = true;
     }
 
-    public void sethCosts(AbstractNode endNode) {
-        this.sethCosts((absolute(this.getxPosition() - endNode.getxPosition())
-                + absolute(this.getyPosition() - endNode.getyPosition()))
+    public int getfCosts() {
+        return gCosts + hCosts;
+    }
+
+    public void setgCosts(Node previousNode, int basicCost) {
+        setgCosts(previousNode.getgCosts() + basicCost);
+    }
+
+    public void setgCosts(Node previousNode) {
+        setgCosts(previousNode, BASICMOVEMENTCOST);
+    }
+
+    public int calculategCosts(Node previousNode) {
+        return (previousNode.getgCosts()
+                + BASICMOVEMENTCOST);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Node other = (Node) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + this.x;
+        hash = 17 * hash + this.y;
+        return hash;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isWalkable() {
+        return walkable;
+    }
+
+    public void setWalkable(boolean walkable) {
+        this.walkable = walkable;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public int getgCosts() {
+        return gCosts;
+    }
+
+    public void setgCosts(int gCosts) {
+        this.gCosts = gCosts;
+    }
+
+    public void sethCosts(int hCosts) {
+        this.hCosts = hCosts;
+    }
+
+    public void sethCosts(Node endNode) {
+        this.sethCosts((absolute(this.getX() - endNode.getX())
+                + absolute(this.getY() - endNode.getY()))
                 * BASICMOVEMENTCOST);
     }
 
     private int absolute(int a) {
         return a > 0 ? a : -a;
     }
-
 }
---
+++ src/test/java/finder/MapTest.java
@@ -0,0 +1,68 @@
+package finder;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import parser.Parser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MapTest {
+    private static final Logger LOG = LoggerFactory.getLogger(MapTest.class);
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+        private void logInfo(Description description, long nanos) {
+            LOG.info(String.format("+++ Test %s spent %d ms",
+                    description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos)));
+        }
+
+        @Override
+        protected void finished(long nanos, Description description) {
+            logInfo(description, nanos);
+        }
+    };
+    private static Map map;
+
+    @BeforeClass
+    public static void init() throws IOException {
+        map = new Map(Parser.parse());
+    }
+
+    @Test
+    public void findPathFromParsedTable() throws Exception {
+        List<Node> path = map.findPath();
+
+        for (Node aPath : path) {
+            System.out.print("(" + aPath.getX() + ", " + aPath.getY() + ") -> ");
+        }
+
+        System.out.println("\nNumber of steps: " + path.size());
+
+    }
+
+    @Test
+    public void findPathSimple() throws Exception {
+        int[][] matrix = new int[][]{
+                {-2, -1, 0, 0, 0},
+                {0, -1, -1, -1, -3},
+                {0, 0, 0, -1, 0},
+        };
+        Map map = new Map(matrix);
+        map.drawMap();
+        List<Node> path = map.findPath();
+
+        for (Node aPath : path) {
+            System.out.print("(" + aPath.getX() + ", " + aPath.getY() + ") -> ");
+        }
+
+        System.out.println("\nNumber of steps: " + path.size());
+
+    }
+
+}
